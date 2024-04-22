86bb2f1baa95760c1ebd9b6f8a6b47942e1fbd44
Brandon Short <bshortdev@gmail.com>
2024-04-22 05:58:21 -0400

    Scaffolded a Vite Vue TypeScript project. Added password length slider to App.vue

---
+++ password-sorcerer/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const passwordLength = ref(0);
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Password Sorcerer</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <label for="passwordLengthRange" class="form-label">Password Length</label>
+      </div>
+      <div class="col">
+        <input type="range" class="form-range" min="0" max="5" id="passwordLengthRange" v-model="passwordLength">
+        <div>{{passwordLength}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
