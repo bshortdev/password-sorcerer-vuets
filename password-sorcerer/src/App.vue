69459883995181fb6ac1ccc6f0356915e49bc084
Brandon Short <bshortdev@gmail.com>
2024-04-28 22:21:54 -0400

    Added comment explaining shuffle algorithm.

--- password-sorcerer/src/App.vue
+++ password-sorcerer/src/App.vue
@@ -51,7 +51,7 @@ const getRandomSpecialCharacter = () => {
   return String.fromCharCode(specialCharacterAsciiStart + letterIndex);
 }
 
-const shuffle = (array: string[]) => {
+const shuffle = (array: string[]) => { // Fisher-Yates Sorting Algorithm
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
